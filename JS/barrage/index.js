396dd1a073dd07d47e620374b9a57fa185fc8879
王铭顺 <553147551@qq.com>
2021-05-02 21:49:39 +0800

    52

--- JS/barrage/index.js
+++ JS/barrage/index.js
@@ -6,6 +6,14 @@ let $btn = doc.getElementById('btn')
 let $color = doc.getElementById('color')
 let $range = doc.getElementById('range')
 
+let data = [
+  {value: '麻烦给我爱人来一杯mojito', time: 5, color: 'red', speed: 1, fontSize: 22},
+  {value: '听妈妈的话', time: 10, color: 'green', speed: 1, fontSize: 30},
+  {value: '听话，晚点再恋爱吧！', time: 6},
+  {value: '晚点再恋爱吧！', time: 15},
+  {value: '再恋爱吧！', time: 20},
+  {value: '恋爱吧！', time: 18}
+]
 创建CanvasBarrage类
 class CanvasBarrage {
   // ={} 不传值，则默认为空对象（可以不传） 
@@ -20,6 +28,9 @@ class CanvasBarrage {
 
     // 创建画布
     this.ctx = canvas.getContext('2d')
-    
+    // 设置默认的参数，如果没传就带上
+    let defOpts = {
+      
+    }
   }
 }
