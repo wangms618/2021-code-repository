77e751ad442ece60e6679f3196caca06b78a7f1e
王铭顺 <553147551@qq.com>
2021-05-07 11:27:44 +0800

    leetcode070

---
+++ LEETCODE/070/70.js
@@ -0,0 +1,12 @@
+// var climbStairs = function(n) {
+//   var a = [1,2]
+//   for(let i=2;i<n;i++){
+//       a.push(a[i-1]+a[i-2])
+//   }
+//   return a[n-1]
+// };
+
+// 如果创建函数或创建局部变量时，在栈上面申请空间，在内存的高地址分配
+var climbStairs = function(n) {
+    
+};
