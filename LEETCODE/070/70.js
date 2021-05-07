d6e6f999e4fadb0be55eaa56c8df54d88bb83ee3
王铭顺 <553147551@qq.com>
2021-05-07 12:25:43 +0800

    70

--- LEETCODE/070/70.js
+++ LEETCODE/070/70.js
@@ -5,6 +5,18 @@
 //   }
 //   return a[n-1]
 // };
+// 优化，用三个变量求解
+
+// 尾递归
+// var climbStairs = function(n,n1=1,n2=2) {
+//   if(n==1){
+//       return n1
+//   }
+//   if(n==2){
+//       return n2
+//   }
+//   return climbStairs(n-1,n2,n1+n2)
+// };
 
 // 如果创建函数或创建局部变量时，在栈上面申请空间，在内存的高地址分配
 var climbStairs = function(n) {
