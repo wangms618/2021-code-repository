18b5218cafe2a95cab829c249330de2702413e29
王铭顺 <553147551@qq.com>
2021-05-12 23:38:07 +0800

    shopping

--- LEETCODE/053/53.js
+++ LEETCODE/053/53.js
@@ -1,23 +1,35 @@
 // 子数组从长度1开始遍历数组，求出每个数组和的最大值，直到长度最大
-var maxSubArray = function (nums) {
-  var max1 = nums[0];
-  var max = nums[0];
-  let len = nums.length;
-  let num = 0;
-  // 子数组长度
-  for (let i = 0; i < len; i++){
-    for (let j = i; j < len; j++){
-      num += nums[j]
-      if (num > max) {
-        max = num
-      }
-    }
-    if (max>max1) {
-      max1 = max;
-    }
-    num = 0;
-  }
-  return max1
-};
+// var maxSubArray = function (nums) {
+//   var max1 = nums[0];
+//   var max = nums[0];
+//   let len = nums.length;
+//   let num = 0;
+//   // 子数组长度
+//   for (let i = 0; i < len; i++){
+//     for (let j = i; j < len; j++){
+//       num += nums[j]
+//       if (num > max) {
+//         max = num
+//       }
+//     }
+//     if (max>max1) {
+//       max1 = max;
+//     }
+//     num = 0;
+//   }
+//   return max1
+// };
 
-maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+// dp(n) = nums[n]+max(dp[n-1],0)
+var maxSubArray = function (nums) {
+  if (nums.length == 0) {
+    return 0
+  }
+  let dp = [nums[0]]
+  for (let i = 1; i < nums.length; i++) {
+    dp.push(nums[i] + Math.max(dp[i - 1], 0))
+  }
+  return Math.max(...dp)
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
--- jsdata/Scope/index.js
+++ jsdata/Scope/index.js
@@ -1,19 +1,30 @@
-function test() {
-  var arr = []
-  for (var i = 0; i < 10; i++) {
-    (function(j) {
-      arr[j] = function() {
-        console.log(j);
-      }
-    })(i)
+// function test() {
+//   var arr = []
+//   for (var i = 0; i < 10; i++) {
+//     (function(j) {
+//       arr[j] = function() {
+//         console.log(j);
+//       }
+//     })(i)
+//   }
+//   return arr
+// }
+
+// var myArr = test()
+
+
+
+// for (var j = 0; j < 10; j++) {
+//   myArr[j]()
+// }
+
+
+function foo() {
+  var a = 2
+  function bar() {
+    console.log(a);
   }
-  return arr
+  return bar
 }
-
-var myArr = test()
-
-
-
-for (var j = 0; j < 10; j++) {
-  myArr[j]()
-}
+var baz = foo()
+baz() // 2 这就是闭包的效果
