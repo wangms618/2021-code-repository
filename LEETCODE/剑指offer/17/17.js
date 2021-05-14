8e8cf2b8ed8317e45dc29db2c43cb24eecc8a74a
王铭顺 <553147551@qq.com>
2021-05-14 10:48:03 +0800

    微信

--- LEETCODE/剑指offer/17/17.js
+++ LEETCODE/剑指offer/17/17.js
@@ -1,3 +1,17 @@
+// 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
 var printNumbers = function (n) {
-  
+  // let arr = []
+  // for (let i = 1; i < 10 ** n; i++) {
+  //   arr.push(i)
+  // }
+  // return arr
+  let num = 1
+  let arr = []
+  while (num.toString().length <= n) {
+    arr.push(num)
+    num++
+  }
+  return arr
 };
+console.log(printNumbers(2));
+// var len = n.toString().length
