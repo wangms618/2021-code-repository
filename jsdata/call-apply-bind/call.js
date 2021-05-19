e8a9d09f5295aff94f28bf740c393b7e02121747
王铭顺 <553147551@qq.com>
2021-05-19 21:34:28 +0800

    call

---
+++ jsdata/call-apply-bind/call.js
@@ -0,0 +1,27 @@
+// 将要改变this指向的方法挂到目标this上执行并返回
+var a = {
+  name: '金总'
+}
+// var fn = function () {
+//   console.log(this.name);
+// }
+// fn.call(a)
+
+
+Function.prototype.mycall = function (context) {
+  if (typeof this !== 'function') {
+    throw new Error('not function')
+  }
+  context = context || window
+  context.fn = this
+  let arg = [...arguments].slice(1)
+  let result = context.fn(...arg)
+  delete context.fn
+  return result
+}
+
+function test(x, y, z) {
+  console.log(this.name)
+  console.log(x, y, z);
+}
+test.mycall(a, 1, 2, 3)
