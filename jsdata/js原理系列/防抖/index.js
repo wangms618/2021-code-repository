7342ed145d95d4500975098c99cd0ebbdc88d926
王铭顺 <553147551@qq.com>
2021-06-04 15:19:51 +0800

    防抖1

---
+++ jsdata/js原理系列/防抖/index.js
@@ -0,0 +1,13 @@
+// 防止抖动:在规定的时间内，没有触发第二次，则执行
+
+function debounce(fn, delay) {
+  let timer = null
+  return function () {
+    let arg = arguments
+    let context = this
+    clearTimeout(timer)
+    timer = setTimeout(function () {
+      fn.apply(context,arg)
+    },delay)
+  }
+}
