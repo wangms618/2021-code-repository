e050004a00de512b607120abd998b819a39d7f11
王铭顺 <553147551@qq.com>
2021-06-06 23:37:22 +0800

    2337

---
+++ wxApp/letter/miniprogram/pages/home/home.js
@@ -0,0 +1,91 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:[
+      '../../images/lunbo1.png',
+      '../../images/lunbo2.jpg',
+      '../../images/lunbo3.png'
+    ],
+    imgSelect:[{
+      imgUrl:'../../images/food.png',
+      selectName:'美食'
+    },
+    {
+      imgUrl:'../../images/idea.png',
+      selectName:'创意',
+    },
+    {
+      imgUrl:'../../images/fashion.png',
+      selectName:'时尚',
+    },
+    {
+      imgUrl:'../../images/travel.png',
+      selectName:'旅行',
+    },
+    {
+      imgUrl:'../../images/love.png',
+      selectName:'心情'
+    }
+  ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
