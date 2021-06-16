8c2e1c176ba3f7971b20f9c97f647aba9a174ac4
王铭顺 <553147551@qq.com>
2021-06-16 21:34:57 +0800

    2134

---
+++ ES6/异步编程/promise3.js
@@ -0,0 +1,25 @@
+function executor(resolve, reject) {
+  resolve(100)
+}
+
+let demo = new Bromise(executor)
+
+function onResolve(value) {
+  console.log(value);
+}
+
+demo.then(onResolve)
+
+function Bromise(executor) {
+  let onResolve_ = null
+  let onReject_ = null
+  this.then = function (onResolve, onReject) {
+    onResolve_ = onResolve
+  }
+  function resolve(value) {
+    setTimeout(() => {
+      onResolve_(value)
+    }, 0)
+  }
+  executor(resolve, null)
+}
