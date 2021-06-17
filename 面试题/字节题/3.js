19d89c61953dacf6a3df0a12f78aa31e3895d244
王铭顺 <553147551@qq.com>
2021-06-17 17:09:54 +0800

    10

--- 面试题/字节题/3.js
+++ 面试题/字节题/3.js
@@ -4,4 +4,24 @@
 // 3. 箭头函数不能用于构造函数，不能使用new。普通函数可以用于构造函数，创建对象实例
 // 4. 箭头函数不绑定arguments
 // 5. 箭头函数无原型
-``
+
+// new
+function MyNew() {
+  //创建一个空对象
+  let obj = {}
+  //获取构造函数
+  let Con = [].shift.call(arguments)
+  //设置空对象的原型
+  obj.__proto__ = Con.prototype
+  // 使用apply绑定this，执行构造函数
+  let result = Con.apply(obj, arguments)
+  return result instanceof Object ? result : obj
+}
+  
+function Mynew() {
+  let obj = {}
+  let constructor = Array.prototype.shift.call(arguments)
+  obj.__proto__ = constructor.prototype
+  constructor.apply(obj, arguments)
+  return obj
+}
