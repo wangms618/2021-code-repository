54650fb8617510300578eef8c96d101fdf20bff3
王铭顺 <553147551@qq.com>
2021-06-19 20:52:22 +0800

    2052

---
+++ ES6/异步编程/asyncAwait.js
@@ -0,0 +1,22 @@
+// fetch('https://www.baidu.com')
+//   .then((response) => {
+//     console.log(response);
+//     return fetch('https://www.baidu.com/test')
+//   }).then((response) => {
+//     console.log(response);
+//   }).catch((error) => {
+//     console.log(error);
+//   })
+
+
+// async function foo() {
+//   try {
+//     let res1 = await fetch('https://www.baidu.com')
+//     console.log(res1);
+//     let res2 = await fetch('https://www.baidu.com/test')
+//     console.log(res2);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// foo()
