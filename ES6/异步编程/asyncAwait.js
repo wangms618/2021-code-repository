6bca8404b1162992e0aa06fe12f5c56e211b596b
王铭顺 <553147551@qq.com>
2021-06-19 21:46:57 +0800

    5

--- ES6/异步编程/asyncAwait.js
+++ ES6/异步编程/asyncAwait.js
@@ -20,3 +20,64 @@
 //   }
 // }
 // foo()
+
+// 这段代码的返回结果给了async
+// async function bar() {
+//   return 2
+// }
+// console.log(bar()); // Promise { 2 }
+
+await 100
+let promise_ = new Promise((resolve, reject) => {
+  resolve(100)
+})
+
+// promise_.then((a) => {
+//   console.log(a);
+//   console.log(2); 
+//   //....
+// })
+
+
+// async function bar() {
+//   console.log(1);
+//   let a = await 100
+//   console.log(a);
+//   console.log(2);
+// }
+// console.log(0);
+// bar() // 启动 bar ·协程
+// console.log(3);
+//  // 0 1 3 100 2
+
+
+
+async function foo() {
+  console.log('foo')  // 3
+}
+async function bar() {
+  console.log('bar start')  // 2
+  await foo()
+  console.log('bar end') // 6
+}
+console.log('script start') // 1
+setTimeout(function () {
+  console.log('setTimeout')  // 8
+}, 0)
+bar();
+new Promise(function (resolve) {
+  console.log('promise executor')  // 4
+  resolve();
+}).then(function () {
+  console.log('promise then')  // 7
+})
+console.log('script end')  // 5
+
+// script start
+// bar start
+// foo
+// promise executor
+// script end
+// bar end
+// promise then
+// setTimeout
