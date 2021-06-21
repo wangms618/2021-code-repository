4491bf9b65d88e6a3d7a29c19214484b68f66a74
王铭顺 <553147551@qq.com>
2021-06-21 21:01:26 +0800

    2101

--- node/koa-demo/demos/demo07.js
+++ node/koa-demo/demos/demo07.js
@@ -1,16 +1,33 @@
 const Koa = require('koa')
 const app = new Koa()
-const logger = (ctx, next) => {  // 中间键
+// const route = require('koa-route')
+const logger = (ctx,next) => { // 中间件
+  // ctx.response.body = 'hello world'
   console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
+  // next()使main函数生效，否则main函数执行失败
   next()
 }
+
 const main = (ctx) => {
-  ctx.response.body = 'Hello World'
+  ctx.response.body = 'hello world'
 }
 
 app.use(logger)
 app.use(main)
 
+
+
+
+
+
+// const a = (ctx) => {
+//   ctx.body = 'a页面'
+// }
+// app.use(route.get('/a', a))
+// app.use((ctx) => {
+//   ctx.body = '你好'
+// })
+
 app.listen(3000, () => {
-  console.log('服务器已启动');
+  console.log('服务已启动');
 })
