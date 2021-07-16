f0f5a3c672dd7cd9c8ac6387fe7358365e66441f
王铭顺 <553147551@qq.com>
2021-07-16 15:47:28 +0800

    ts

---
+++ TS/whyTS/1.ts
@@ -0,0 +1,22 @@
+// 定义类型
+export type User = {name:string,age:number,occupation:string};
+
+export const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
--- TS/whyTS/index.ts
+++ TS/whyTS/index.ts
@@ -1,7 +1,7 @@
 /**
  * @description count string size
  */
-export function foo(name: string){
+export function foo(name: string) {
   const separator = ','
   return name.split(separator)
 }
@@ -10,7 +10,25 @@ foo('string')
 
 
 // 什么时候用泛型
+// 普通
+// function bar(arg: number | string): number | string { // 不够严谨，传number返回string怎么办
+//   return arg
+// }
+// 泛型
+// 这个函数会返回任何传入它的值。 你可以把这个函数当成是 echo命令。
+// function bar<T>(arg: T): T {
+//   return arg
+// }
 
-function bar(arg: number) {
-  return arg
+
+// 类型定义在哪
+export type fooItem = string | number | null // 用type创造类型
+function baz(arg: fooItem):void {
+  console.log(arg);
 }
+export interface useRes{  // useRes必须包含nickname和age类型，avatar不一定有
+  nickname: string,
+  avatar?: string,
+  age:number
+}
+
