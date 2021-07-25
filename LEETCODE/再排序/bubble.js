554d3eb2e2d7894bc1e50a56160a36b109120bc9
王铭顺 <553147551@qq.com>
2021-07-25 20:23:01 +0800

    bubble

---
+++ LEETCODE/再排序/bubble.js
@@ -0,0 +1,17 @@
+arr = [3, 5, 2, 1, 4]
+// 冒泡
+function bubble(arr) {
+  let len = arr.length
+  for (let i = 0; i < len; i++){
+    let flag = true
+    for (let j = 0; j < len - i-1; j++){
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        flag = false
+      }
+    }
+    if (flag) break
+  }
+  return arr
+}
+console.log(bubble(arr));
