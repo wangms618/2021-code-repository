db86def1bbee0e079f5921a123a7a63e4950a8ce
王铭顺 <553147551@qq.com>
2021-08-03 18:54:33 +0800

    cli实现

---
+++ vue/vuecli实现/cli/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div><h1>1111</h1>你好我是牛牛牛</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const testFunction = () => {
+      console.log("测试一下babel");
+    };
+    return {
+      testFunction,
+    };
+  },
+};
+</script>
+
+<style>
+div {
+  color: yellowgreen;
+}
+</style>
