89e89758eafc9ceab8fedc468c04be471c2142ab
王铭顺 <553147551@qq.com>
2021-08-17 15:38:47 +0800

    模块化

--- react/jianshu/src/TodoList.js
+++ react/jianshu/src/TodoList.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react'
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store/index.js';
-// const data = [
-//   'Racing car sprays burning fuel into crowd.',
-//   'Japanese princess to wed commoner.',
-//   'Australian walks 100km after outback crash.',
-//   'Man charged over missing wedding girl.',
-//   'Los Angeles battles huge wildfires.',
-// ];
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes'
+import { getInputChangeAction,getAddItemAction,getDeleteItemAction } from './store/actionsCreaters'
+
 class TodoList extends Component {
   constructor(props) {
     super(props)
@@ -17,6 +13,8 @@ class TodoList extends Component {
     this.state = store.getState()
     this.hendleInputChange = this.hendleInputChange.bind(this)
     this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+    // this.handleItemDelete = this.handleItemDelete.bind(this)
     store.subscribe(this.handleStoreChange) // 响应式取store数据
   }
   render() {
@@ -24,14 +22,14 @@ class TodoList extends Component {
       <div style={{ marginTop: '10px', marginLeft: '10px' }}>
         <div>
           <Input type="text" onChange={this.hendleInputChange} value={this.state.inputValue} style={{ width: '300px', marginRight: '10px' }} placeholder="todo info" />
-          <Button type="primary">提交</Button>
+          <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
         </div>
         <List
-          style={{marginTop:'10px',width:'300px'}}
+          style={{ marginTop: '10px', width: '300px' }}
           bordered
           dataSource={this.state.list}
-          renderItem={item => (
-            <List.Item>
+          renderItem={(item, index) => (
+            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
               {item}
             </List.Item>
           )}
@@ -41,10 +39,11 @@ class TodoList extends Component {
   }
   hendleInputChange(e) {
     // 创建一个action
-    const action = {
-      type: 'change_input_value',
-      value:e.target.value
-    }
+    // const action = {
+    //   type: CHANGE_INPUT_VALUE,
+    //   value: e.target.value
+    // }
+    const action = getInputChangeAction(e.target.value)
     // dispatch会使action被store感知到
     store.dispatch(action)
     // console.log(e.target.value);
@@ -53,6 +52,21 @@ class TodoList extends Component {
   handleStoreChange() {
     // store.getState()
     this.setState(store.getState())
+  }
+  handleBtnClick() {
+    // const action = {
+    //   type: ADD_TODO_ITEM,
+    // }
+    const action = getAddItemAction()
+    store.dispatch(action)
+  }
+  handleItemDelete(index) {
+    // const action = {
+    //   type: DELETE_TODO_ITEM,
+    //   index
+    // }
+    const action = getDeleteItemAction(index)
+    store.dispatch(action)
   }
 }
 
--- react/jianshu/src/store/reducer.js
+++ react/jianshu/src/store/reducer.js
@@ -1,17 +1,29 @@
+import { CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DELETE_TODO_ITEM } from './actionTypes' 
 // 管理store里的数据
 const defaultState = {
-  inputValue: '123',
-  list:[1,2,3]
+  inputValue: '',
+  list:[]
 }
 // reducer 可以接受state ，但是不能修改state
 export default (state = defaultState, action) => {
-  if (action.type === 'change_input_value') {
+  if (action.type === CHANGE_INPUT_VALUE) {
     // state.inputValue
     const newState = JSON.parse(JSON.stringify(state))
     newState.inputValue = action.value
     return newState
-    console.log(state,action);
-    }
+    // console.log(state,action);
+  }
+  if (action.type === ADD_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+  if (action.type === DELETE_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.splice(action.index,1)
+    return newState
+  }
   return state
 }
 
