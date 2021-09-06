f9c36a196d9b442c4fc9924960e593d162f75ae1
王铭顺 <553147551@qq.com>
2021-09-06 16:51:54 +0800

    复习

--- 基础复习/浅拷贝和深拷贝/浅拷贝.js
+++ 基础复习/浅拷贝和深拷贝/浅拷贝.js
@@ -25,14 +25,14 @@
 // console.log(obj2);
 // console.log(obj1);
 
-// let arr = [1, 3, {
-//   username: 'kobe'
-// }];
-// let arr2 = arr.concat();
-// arr2[2].username = 'wade';
-// arr2[1] = 2
-// console.log(arr); //[ 1, 3, { username: 'wade' } ]
-// console.log(arr2); // [ 1, 2, { username: 'wade' } ]
+let arr = [1, 3, {
+  username: 'kobe'
+}];
+let arr2 = arr.concat();
+arr2[2].username = 'wade';
+arr2[1] = 2
+console.log(arr); //[ 1, 3, { username: 'wade' } ]
+console.log(arr2); // [ 1, 2, { username: 'wade' } ]
 
 
 
@@ -58,12 +58,11 @@ function shallowClone(source) {
 console.log('obj1',obj1) // obj1 { name: '浪里行舟', arr: [ 1, [ 5, 6, 7 ], 4 ] }
 console.log('obj3',obj3) // obj3 { name: '阿浪', arr: [ 1, [ 5, 6, 7 ], 4 ] }
 
-
-// function shallClone(obj) {
-//   let target = {}
-//   for (var i in obj) {
-//     if (obj.hasOwnProperty(i)) {
-//       target[i] = obj[i]
+// function clone(obj) {
+//   var target = {}
+//   for (let item in obj) {
+//     if (obj.hasOwnProperty(item)) {
+//       target[item] = obj[item]
 //     }
 //   }
 //   return target
--- 基础复习/浅拷贝和深拷贝/深拷贝.js
+++ 基础复习/浅拷贝和深拷贝/深拷贝.js
@@ -1,4 +1,3 @@
-
 // let arr = [1, 3, {
 //   username: ' kobe'
 // }];
@@ -18,18 +17,40 @@
 // console.log(arr, arr4)
 
 // 简单版
-function deepClone(source) {
-  if (typeof source === 'object') {
-    let target = Array.isArray(target) ? [] : {}
-    for (let key in source) {
-      if (source.hasOwnProperty(key)) {
-        target[key] = deepClone(source[key])
+// function deepClone(source) {
+//   if (typeof source === 'object') {
+//     let target = Array.isArray(target) ? [] : {}
+//     for (let key in source) {
+//       if (source.hasOwnProperty(key)) {
+//         target[key] = deepClone(source[key])
+//       }
+//     }
+//     return target
+//   } else {
+//     return source
+//   }
+// }
+
+// 考虑数组
+function deeoClone(obj) {
+  if (typeof obj === 'object') {
+    let target = Array.isArray(obj) ? [] : {}
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        target[key] = deeoClone(obj[key])
       }
     }
     return target
   } else {
-    return source
+    return obj
   }
 }
+let arr = [1, 3, {
+  username: ' kobe'
+}];
 
-// 考虑数组
+let arr4 = deeoClone(arr)
+arr4[2].username = 'duncan';
+arr4[1] = 2
+console.log('arr', arr);
+console.log('arr4', arr4)
