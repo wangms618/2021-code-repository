25b0d281df8363537d39df011339cc52274d605c
王铭顺 <553147551@qq.com>
2021-09-12 11:13:59 +0800

    egg

---
+++ react/React-egg/diary/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd-mobile';
+function App(){
+  return (
+    <div>
+      <Button type="primary">测试</Button>
+    </div>
+  )
+}
+export default App
