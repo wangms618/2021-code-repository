dcce2b2231e98714199f06ae42609d65834a9335
王铭顺 <553147551@qq.com>
2021-09-18 23:03:52 +0800

    ajax

--- 面试题/BOM/readme.md
+++ 面试题/BOM/readme.md
@@ -7,4 +7,16 @@ BOM（browser object model）；浏览器对象模型，提供一些属性和方
 4. navigator
 5. window.screen(用户信息)
 
-# 以下代码输出什么
+# 以下代码输出什么?
+通过var往window上添加的属性会有一个名为
+`[[configurable]] == false` 不可配置
+
+
+# 你知道 间歇调用 和 超时调用 吗？
+超时调用：setTimeout
+间歇调用：setInterval
+
+# 为什么说少用setInterval，尽量用setTimeout代替
+
+# hash和history
+history是h5提供的，replaceState,pushState用于修改url的方法，通过popState监听url变化
--- 面试题/vue2-vue3/vue3.js
+++ 面试题/vue2-vue3/vue3.js
@@ -3,39 +3,48 @@
 let toProxy = new WeakMap() // 原对象 ： 代理过的对象
 let toRaw = new WeakSet() // 代理过的对象 : 原对象
 
+
+// 判断是否是val对象
 function isObject(val) {
   return typeof val === 'object' && val !== null
 }
 
-
+// 第一步
+// 这里的想法是拿到target并将它变成响应式的
 function reactive(target) {
   // 创建响应式对象
   return createReactiveobject(target)
 }
 
+// 第二步
 function createReactiveobject(target) {
-  if (!isObject(target)) { // 如果不是对象的话，直接返回
+  //源码里如果不是对象的话，用ref变成响应式，这里先写成如果不是对象的话，直接返回，
+  if (!isObject(target)) { // 判断target是否是一个对象
     return target
   }
+  // 如果是一个对象，给这个对象做代理
   let proxy = toProxy.get(target)
+  // 如果被代理过，直接return
   if (proxy) {
     return proxy
   }
+  // Proxy的第二个参数
   let baseHandler = {
     get(target, key, receiver) {
       console.log('获取');
+      // Reflect.get代理，只有取值的那一刻发现是对象才递归
       let result = Reflect.get(target, key, receiver)
       // 取到的那一刻发现是对象才去做下一次递归
       return isObject(result) ? reactive(result) : result // 递归多层代理
     },
     set(target, key, value, receiver) {
-      // console.log('新增');
-      // 不存在直接植入，新增的属性可以劫持
+      // 查看原对象是否有这个键key
       let hadKey = target.hasOwnProperty(key)
       let oldValue = target[key]
+      // 如果原对象没有这个键key，说明是新增
       if (!hadKey) {
         console.log('新增');
-      } else if (oldValue !== value) { // 旧值不等于新值
+      } else if (oldValue !== value) { // 已有这个键，旧值不等于新值
         console.log('修改');
       }
       // 将代理好的对象内部的值改动
@@ -43,6 +52,7 @@ function createReactiveobject(target) {
       return res
     }
   }
+  // 第三步让target被代理，baseHandler拿出去写，因为baseHandler是一个对象
   let observed = new Proxy(target, baseHandler) // 代理target
   toProxy.set(target, observed)
   toRaw.add(observed,target)
